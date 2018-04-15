9ec47a322a1bc1add02d3595f9d0d79c245366b0
Unknown <chilled-1337@hotmail.com>
2018-04-15 13:37:21 +0200

    Revert "Updated verifyer"
    This reverts commit b65062c1bc82c8dff92c76880fde6f3aed5abded.

--- js/partial/level_verifyer.js
+++ js/partial/level_verifyer.js
@@ -1,15 +1,1 @@
 // JavaScript source code
-var miss = "is missing";
-var isOK = "is valid entry";
-function nameCheck() {
-    if (!level.name) {
-        console.log("Level Name" + miss);
-    } else {
-        console(level.name + isOK);
-    }
-    if (!level.buttons[0].name) {
-        console.log("Button name" + miss);
-    } else {
-        console.log("Name" + isOK);
-    }
-}
