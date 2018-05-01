f0b361b631339eae044cac35f93a4dc82f782e6f
Trym <chilled-1337@hotmail.com>
2018-05-01 12:14:07 +0200

    added load player

--- js/partial/local_storage.js
+++ js/partial/local_storage.js
@@ -4,7 +4,13 @@ function save_player() {
    // var player_saved = { score:Player.score, spt:Player.score_per_tick, inventory:Player.inventory};
 
     localStorage.setItem("object", JSON.stringify(Player));
+    return "saved";
 }
 function save_game(){
     var game_save = {}
+}
+
+function load_player(){
+    JSON.parse(localStorage.getItem("object",Player));
+    return "Game loaded";
 }
