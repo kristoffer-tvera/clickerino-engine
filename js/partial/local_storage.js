0931cd1aab147f6fd53cbaf4a3133909deb28e42
Trym <chilled-1337@hotmail.com>
2018-05-01 11:54:19 +0200

    Updated playering saving again

--- js/partial/local_storage.js
+++ js/partial/local_storage.js
@@ -1,9 +1,9 @@
 // JavaScript source code
 
 function save_player() {
-    var player_saved = { score:Player.score, spt:Player.score_per_tick, inventory:player.inventory};
+   // var player_saved = { score:Player.score, spt:Player.score_per_tick, inventory:Player.inventory};
 
-    localStorage.setItem("object", JSON.stringify(player_saved));
+    localStorage.setItem("object", JSON.stringify(Player));
 }
 function save_game(){
     var game_save = {}
