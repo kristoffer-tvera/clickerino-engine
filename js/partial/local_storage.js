027646c3f41e2ab07df23cf2b85f77594091dc17
Trym <chilled-1337@hotmail.com>
2018-05-01 12:52:59 +0200

    updated functions for loading and saving

--- js/partial/local_storage.js
+++ js/partial/local_storage.js
@@ -1,16 +1,41 @@
 // JavaScript source code
+function save_game(){
+    save_player();
+    save_level();
+}
+
+function load_game(){
+    load_player();
+    load_level();
+}
 
 function save_player() {
-   // var player_saved = { score:Player.score, spt:Player.score_per_tick, inventory:Player.inventory};
-
-     localStorage.setItem("Player Save", JSON.stringify(Player));
-    return "saved";
-}
-function save_game(){
-    var game_save = {}
+    save_to_localstorage("player",Player);
 }
 
-function load_player(){
-    var Player = JSON.parse(localStorage.getItem("Player Save"));
-    return "Game loaded";
+function load_player(name){
+    var fromLocalStorage = load_from_localstorage("player");
+    if (fromLocalStorage){
+        Player = fromLocalStorage;
+    }
+}
+
+function save_level(){
+    save_to_localstorage("level", Level);
+}
+
+function load_level(){
+    var fromLocalStorage = load_from_localstorage("level");
+    if (fromLocalStorage){
+        Level = fromLocalStorage;
+    }
+}
+
+function load_from_localstorage(keyName){
+    return JSON.parse(localStorage.getItem(keyName));
+}
+
+
+function save_to_localstorage(keyName, objectToSave){
+    localStorage.setItem(keyName, JSON.stringify(objectToSave));
 }
